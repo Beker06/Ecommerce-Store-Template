4a0e84e825bbaa043f61147e1924a90c0ca44d0d
Eliam Vazquez <98252478+Beker06@users.noreply.github.com>
2024-02-13 03:02:36 -0800

    [V0.1.0] Setup and Homepage

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+      <footer className="bg-white dark:bg-[#18191A] border-t border-gray-200/30">
+        <div className="mx-auto py-10">
+          <p className="text-center text-xs text-black dark:text-gray-200">
+            &copy; 2024 Beker Template Store, Inc. All rights reserved.
+          </p>
+        </div>
+      </footer>
+    )
+  };
+  
+  export default Footer;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            "tailwindui.com",
+            "res.cloudinary.com"
+        ]
+    }
+};
 
 export default nextConfig;
